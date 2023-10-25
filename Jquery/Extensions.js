44beb49484d43d6c4604673459ae13f1ff772f6c
aodlichao2012-lnw <52318749+aodlichao2012-lnw@users.noreply.github.com>
2023-10-25 10:26:08 +0700

    2

--- Jquery/Extensions.js
+++ Jquery/Extensions.js
@@ -16,4 +16,38 @@ $(document).ready(function(){
             }
         };
     }
+    function CreateTables(headers , values , tablename , classname){
+            // สร้างตาราง
+            var table = $("<table>");
+            table.addClass(classname)
+            // สร้างหัวของตาราง
+            var thead = $("<thead>");
+            thead.addClass(classname)
+            var headerRow = thead.appendTo("<tr>");
+            // var headers = ["Header 1", "Header 2", "Header 3"];
+            for (var i = 0; i < headers.length; i++) {
+                var headerCell = $("<th>");
+                headerCell.appendTo( headers[i]);
+                headerRow.appendTo(headerCell);
+            }
+            // สร้างเนื้อของตาราง
+            var tbody = $("<tbody>");
+            tbody.addClass(classname)
+            for (var i = 1; i <= values.length; i++) {
+                var row = tbody.append("<tr>");
+                for (var j = 1; j <= values.length; j++) {
+                    var cell = row.appendTo("<td>");
+                    cell.txt(values[i][j]) 
+                }
+            }
+            // เพิ่มหัวและเนื้อของตารางเข้ากับตาราง
+            table.appendTo(thead);
+            table.appendTo(tbody);
+            // เพิ่มตารางลงในเอกสาร
+            var tableContainer = $(tablename);
+            tableContainer.appendTo(table);
+    }
+    function toggle(myElement){
+        $(myElement).toggle();
+    }
 })
--- Vanlia/Extensions.js
+++ Vanlia/Extensions.js
@@ -16,3 +16,48 @@ function openModal(){
         }
     };
 }
+function CreateTables(headers , values , tablename , classname){
+    document.addEventListener("DOMContentLoaded", function() {
+        // สร้างตาราง
+        var table = document.createElement("table");
+        table.className = classname;
+        // สร้างหัวของตาราง
+        var thead = document.createElement("thead");
+        thead.className = classname;
+        var headerRow = thead.insertRow();
+        // var headers = ["Header 1", "Header 2", "Header 3"];
+        for (var i = 0; i < headers.length; i++) {
+            var headerCell = document.createElement("th");
+            headerCell.textContent = headers[i];
+            headerRow.appendChild(headerCell);
+        }
+
+        // สร้างเนื้อของตาราง
+        var tbody = document.createElement("tbody");
+        tbody.className = classname;
+        for (var i = 1; i <= 3; i++) {
+            var row = tbody.insertRow();
+            for (var j = 1; j <= 3; j++) {
+                var cell = row.insertCell();
+                cell.textContent = values[i][j];
+            }
+        }
+
+        // เพิ่มหัวและเนื้อของตารางเข้ากับตาราง
+        table.appendChild(thead);
+        table.appendChild(tbody);
+
+        // เพิ่มตารางลงในเอกสาร
+        var tableContainer = document.getElementById(tablename);
+        tableContainer.appendChild(table);
+    });
+}
+function toggle(myElement){
+    var element = document.getElementById(myElement);
+
+if (element.style.display === "none") {
+    element.style.display = "block";
+} else {
+    element.style.display = "none";
+}
+}
